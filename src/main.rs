f332ce92b744580a63704d64950aa7bc1d4ef3f1
dcarlus <denis.carlus@hotmail.fr>
2020-06-30 20:33:22 +0200

    Test structures

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
-use std::io ;
-use std::cmp::Ordering ;
-use rand::Rng ;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
 
-fn main() {
-    println!("Guess the number!") ;
-
-    let secret_number = rand::thread_rng().gen_range(1, 101) ;
-
-    loop  {
-        println!("Please input your guess:") ;
-        let mut guess = String::new() ;
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line...") ;
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        } ;
-
-        println!("You guessed: {}", guess) ;
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("Wow, super!") ;
-                break ;
-            }
+impl Rectangle {
+    /// Function to generate a square.
+    fn create_square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
         }
     }
+
+    /// Get the area of the current rectangle.
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    /// Test if the current rectangle can entirely hold another one.
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width > other.width) && (self.height > other.height)
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 50,
+        height: 30
+    } ;
+    println!("The area of a {:#?} is {} square pixels.", rect1, rect1.area()) ;
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 10
+    } ;
+    println!("{:?} contains {:?}? {}", rect1, rect2, rect1.can_hold(&rect2)) ;
+
+    let rect3 = Rectangle::create_square(20) ;
+    println!("{:?} contains {:?}? {}", rect1, rect3, rect1.can_hold(&rect3)) ;
+    println!("{:?} contains {:?}? {}", rect3, rect1, rect3.can_hold(&rect1)) ;
 }
